6c9bec054a8457f075b63a69ee5a2db05f41e0fb
Andreea <suntandreea@protonmail.com>
2024-10-11 17:34:48 +0300

    using props

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import Post from './components/Post.jsx';
 
 function App() {
 
+  const postData = {
+    image: "/spacejoy-YqFz7UMm8qE-unsplash.jpg",
+    title: "Top 5 Living Room Inspirations",
+    desc: "Curated vibrant colors for your living, make it pop & calm in the same time.",
+    tags: ["Interior"]
+
+  }
+
   return (
-    <Post />
+    <Post {...postData} />
   )
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,31 +1,30 @@
-import cover from "../assets/spacejoy-YqFz7UMm8qE-unsplash.jpg";
 import styles from "./Post.module.scss";
 
-export default function Post() {
+export default function Post({image, title, desc, tags}) {
 
   return (
     <article className={styles.post}>
 
-      <img className={styles["post-cover"]} src={cover} alt="A nice image representing the article" />
+      <img className={styles["post-cover"]} src={image} alt={title} />
 
       <section className={styles["post-content"]}>
         <header className={styles["post-header"]}>
           <div className={styles["post-chip"]}>
-            <span>Interior</span>
+            {tags.map((tag, idx) => <span key={idx}>{tag}</span>)}
           </div>
 
-          <h2 className={`${styles["post-title"]} ${styles.truncate}`}>Top 5 Living Room Inspirations</h2>
+          <h2 className={`${styles["post-title"]} ${styles.truncate}`}>{title}</h2>
         </header>
 
         <aside className={styles["post-summary"]}>
-          <p className={styles["post-description"]}>Curated vibrant colors for your living, make it pop & calm in the same time. </p>
+          <p className={styles["post-description"]}>{desc}</p>
           <button className={styles["post-btn"]}>
             <a href="#">Read more</a>
             <span>
               <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M10.9763 6.16658L6.50625 1.69657L7.68475 0.518066L14.1666 6.99992L7.68475 13.4817L6.50625 12.3032L10.9763 7.83325H0.833252V6.16658H10.9763Z"
-                   />
+                />
               </svg>
             </span>
           </button>
