6332365eb15eea9ceeffc5af8aa416de58a3535e
Andreea <suntandreea@protonmail.com>
2024-10-11 17:48:26 +0300

    fixed image url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Post from './components/Post.jsx';
 function App() {
 
   const postData = {
-    image: "/spacejoy-YqFz7UMm8qE-unsplash.jpg",
+    image: "/greatfrontend-blog-card/spacejoy-YqFz7UMm8qE-unsplash.jpg",
     title: "Top 5 Living Room Inspirations",
     desc: "Curated vibrant colors for your living, make it pop & calm in the same time.",
     tags: ["Interior"]
